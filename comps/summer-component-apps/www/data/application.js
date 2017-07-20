e3ef5fef9a0945db4ba59b757398f56def7ae720
xingychb <xingychb@yonnyou.com>
2017-07-20 10:22:17 +0800

    1

---
+++ comps/summer-component-apps/www/data/application.js
@@ -0,0 +1,87 @@
+var ContactData = {
+            "id": "Page3_GridView1",
+            "type": "GridView",
+            "cols": 3,
+            "isSeparationLine": true,
+            "layout": "grid",
+            "gridData": [
+                {
+                    "type": "radius",
+                    "bgcolor": "rgb(76, 181, 255)",
+                    "color": "white",
+                    "display": "navIcon",
+                    "href": "Page2.html",
+                    "icon": "glyphicon glyphicon-remove",
+                    "linkType": "inapp",
+                    "text": "请假"
+                },
+                {
+                    "type": "radius",
+                    "bgcolor": "rgb(254, 155, 207)",
+                    "color": "white",
+                    "href": "javascript:",
+                    "icon": "glyphicon glyphicon-envelope",
+                    "linkType": "inapp",
+                    "text": "审批"
+                },
+                {
+                    "type": "radius",
+                    "bgcolor": "rgb(66, 203, 205)",
+                    "color": "white",
+                    "href": "javascript:",
+                    "icon": "glyphicon glyphicon-pencil",
+                    "linkType": "inapp",
+                    "text": "证明"
+                },
+                {
+                    "type": "radius",
+                    "bgcolor": "rgb(255, 174, 44)",
+                    "color": "white",
+                    "href": "javascript:",
+                    "icon": "glyphicon glyphicon-user",
+                    "linkType": "inapp",
+                    "text": "通讯录"
+                },
+                {
+                    "type": "radius",
+                    "bgcolor": "rgb(76, 181, 255)",
+                    "color": "white",
+                    "display": "navIcon",
+                    "href": "Page2.html",
+                    "icon": "glyphicon glyphicon-remove",
+                    "linkType": "inapp",
+                    "text": "请假"
+                },
+                {
+                    "type": "radius",
+                    "bgcolor": "rgb(254, 155, 207)",
+                    "color": "white",
+                    "href": "javascript:",
+                    "icon": "glyphicon glyphicon-envelope",
+                    "linkType": "inapp",
+                    "text": "审批"
+                },
+                {
+                    "type": "radius",
+                    "bgcolor": "rgb(66, 203, 205)",
+                    "color": "white",
+                    "href": "javascript:",
+                    "icon": "glyphicon glyphicon-pencil",
+                    "linkType": "inapp",
+                    "text": "证明"
+                },
+                {
+                    "type": "radius",
+                    "bgcolor": "rgb(255, 174, 44)",
+                    "color": "white",
+                    "href": "javascript:",
+                    "icon": "glyphicon glyphicon-user",
+                    "linkType": "inapp",
+                    "text": "通讯录"
+                }
+            ],
+            "style": {
+                "background": "#fff",
+                "width": "100%"
+            }
+        }
